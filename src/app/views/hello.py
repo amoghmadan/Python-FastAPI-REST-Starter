1ac986bcb32744ff7915ff4852bfee0babf7d9f4
Amogh Madan <amoghmadaan@gmail.com>
2024-09-11 23:50:45 +0530

    Use domain model in view for API documentation.

--- src/app/views/hello.py
+++ src/app/views/hello.py
@@ -3,11 +3,12 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.dependencies import get_db
+from app.schemas.domain.hello import World
 
 
-async def world(db: AsyncSession = Depends(get_db)) -> dict[str, str]:
+async def world(db: AsyncSession = Depends(get_db)) -> World:
     """Hello, world from DB."""
     stmt = text("SELECT 'World!' AS world;")
     result = await db.execute(stmt)
     row = result.fetchone()
-    return {"hello": row[0]}
+    return World(hello=row[0])
