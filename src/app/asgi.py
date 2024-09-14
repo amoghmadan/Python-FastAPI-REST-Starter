43934c6bc78ca90d56e486f871c5702fdad82970
Amogh Madan <amoghmadaan@gmail.com>
2024-09-14 19:52:12 +0530

    Implement new auto registration for protocol based middlewares.

--- src/app/asgi.py
+++ src/app/asgi.py
@@ -1,18 +1,30 @@
+"""
+ASGI setup for project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+"""
+
 from fastapi import FastAPI
 
 from app import settings
 from app.ext.lifespan import lifespan
-from app.middlewares import database_middleware, exception_middleware
+from app.middlewares import protocol_middlewares
 from app.urls import urlpatterns
 
 
-def get_asgi_application():
+def get_asgi_application() -> FastAPI:
+    """
+    Generate an application to be used by an ASGI server.
+    :return: app, FastAPI
+    """
     app = FastAPI(lifespan=lifespan, **settings.OPENAPI)
 
-    http_middlewares = [database_middleware, exception_middleware]
-    for middleware in http_middlewares:
-        app.middleware("http")(middleware)
+    # Register middlewares.
+    for protocol, middlewares in protocol_middlewares.items():
+        for middleware in middlewares:
+            app.middleware(protocol)(middleware)
 
+    # Register routers.
     for router in urlpatterns:
         app.include_router(router)
 
