16278add0a70cfcbdefc64e3cf29e55f24194827
Amogh Madan <amoghmadaan@gmail.com>
2024-09-14 19:49:07 +0530

    Add types to lifespan.

--- src/app/ext/lifespan.py
+++ src/app/ext/lifespan.py
@@ -1,11 +1,17 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.applications import AppType, Lifespan
 
 from app.ext.db import session_makers
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app: FastAPI) -> Lifespan[AppType]:
+    """
+    Lifespan to load content in application state or perform operations before startup.
+    :param app: FastAPI
+    :return: Lifespan[AppType]
+    """
     app.state.session_makers = session_makers
     yield
